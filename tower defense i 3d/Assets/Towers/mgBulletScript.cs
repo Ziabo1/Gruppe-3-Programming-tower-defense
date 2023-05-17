931574920045b3ef91e06ccbe8db7f8b23534080
Ziabo1 <122011146+Ziabo1@users.noreply.github.com>
2023-05-17 17:03:10 +0200

    små fix

--- tower defense i 3d/Assets/Towers/mgBulletScript.cs
+++ tower defense i 3d/Assets/Towers/mgBulletScript.cs
@@ -4,7 +4,6 @@ public class mgBulletScript : MonoBehaviour
 {
     private Transform target;
     private float speed = 200;
-    public int damage = 50;
     public void Seek(Transform _target)
     {
         target = _target;
@@ -31,15 +30,6 @@ public class mgBulletScript : MonoBehaviour
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
     }
 
-    void Damage (Transform enemy)
-    {
-        Enemy e = enemy.GetComponent<Enemy>();
-        if (e != null)
-        {
-            e.TakeDamage(damage);
-        }
-    }
-    public 
     void HitTarget()
     {
         Debug.Log("WE HIT SOMETHING");
